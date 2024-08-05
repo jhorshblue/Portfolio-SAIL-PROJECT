46599cea6a992af0a2db8f54cb72777d77618c4d
Joshuar Onanuga <jhorshblue@gmail.com>
2024-08-05 02:26:27 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+	const contactForm = document.getElementById('contactForm');
+
+	contactForm.addEventListener('submit', (event) => {
+			event.preventDefault();
+			alert('Thank you for your message!');
+			contactForm.reset();
+	});
+});
